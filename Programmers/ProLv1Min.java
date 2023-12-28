1e885f26a8906da0214612f6ff326a79691dc1c2
김병웅 <quddnddl35@naver.com>
2023-12-28 16:14:31 +0900

    [Programmers]LV.1 최소직사각형

---
+++ Programmers/ProLv1Min.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        for(int i = 0; i < sizes.length;i++){
+            if(sizes[i][0] < sizes[i][1]){
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+        }
+        int MAX1 = 0;
+        int MAX2 = 0;
+        for(int i = 0; i <sizes.length;i++){
+            MAX1 = Math.max(MAX1,sizes[i][0]);
+            MAX2 = Math.max(MAX1,sizes[i][1]);
+        }
+        return MAX1 * MAX2;
+    }
+}
