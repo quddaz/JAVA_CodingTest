97ca343e745f3295bc3310e515f1317e5f0aa8eb
김병웅 <quddnddl35@naver.com>
2023-12-29 15:49:15 +0900

    [Programmers]LV.1 음양 더하기(스트림 공부)

---
+++ Programmers/ProLv1Signs.java
@@ -0,0 +1,8 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        return IntStream.range(0, absolutes.length)
+            .map(i -> signs[i] ? absolutes[i] : -absolutes[i]).sum();
+    }
+}
