59ffcd458a92a3b9fec4d15bc61f282b98b89ff0
김병웅 <quddnddl35@naver.com>
2024-07-07 14:15:15 +0900

    [BOJ]4963번 섬의 개수 DFS S2

---
+++ BOJ/탐색/BOJ4963.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int w, h;
+    static int[][] map;
+    static boolean[][] visited;
+    // 방향 벡터: 상, 하, 좌, 우 및 대각선
+    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) {
+                break;
+            }
+
+            map = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int count = 0; 
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+            
+            System.out.println(count);
+        }
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < h && ny < w && map[nx][ny] == 1 && !visited[nx][ny]) {
+                dfs(nx, ny);
+            }
+        }
+    }
+}
