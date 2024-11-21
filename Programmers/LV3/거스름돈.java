da61bae3bf6a4f3c442c4f33c57956f7befbe4fb
김병웅 <quddnddl35@naver.com>
2024-11-21 18:28:28 +0900

    [Programmers]

---
+++ Programmers/LV3/거스름돈.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] money) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        
+        for(int coin : money){
+            for(int i = coin; i <= n; i++){
+                dp[i] = dp[i] + dp[i - coin];
+            }
+        }
+        
+        return dp[n];
+    }
+}
