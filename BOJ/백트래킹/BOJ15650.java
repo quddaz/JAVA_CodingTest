aa6b41a6230f6b540a7ffbbe40b70d8b1bc68cef
김병웅 <quddnddl35@naver.com>
2024-12-04 18:31:02 +0900

    [BOJ]15650번 N과 M(2) 백트래킹 S3

---
+++ BOJ/백트래킹/BOJ15650.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        solve(0, 1);
+        System.out.println(sb);
+    }
+
+    public static void solve(int depth, int start) {
+        if(depth == m){
+            for(int i = 0; i < m; i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = start; i <= n; i++){
+            arr[depth] = i;
+            solve(depth + 1, i + 1); 
+        }
+    }
+}
