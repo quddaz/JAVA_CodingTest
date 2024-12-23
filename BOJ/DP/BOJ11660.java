7681f6dea461adfba03f4b7016809f0f6aee4c36
김병웅 <quddnddl35@naver.com>
2024-12-23 20:13:31 +0900

    [BOJ]11660번 구간 합 구하기 5 DP S1

--- BOJ/DP/BOJ11660.java
+++ BOJ/DP/BOJ11660.java
@@ -1,34 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        int[][] arr = new int[N + 1][N + 1];
-        int[][] prefixSum = new int[N + 1][N + 1];
-
-        // 배열 값 입력 및 누적합 계산
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                arr[i][j] = sc.nextInt();
-                // 누적 합 계산
-                prefixSum[i][j] = arr[i][j] + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+        int map[][] = new int[n+1][n+1];
+        int dp[][] = new int[n+1][n+1];
+        for(int i = 1; i <= n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n; j++){
+                int temp = Integer.parseInt(st.nextToken());
+                map[i][j] = temp;
+                dp[i][j] = dp[i][j-1] + temp + dp[i-1][j] - dp[i-1][j-1];
             }
         }
 
-        for (int k = 0; k < M; k++) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
-            
-            int result = prefixSum[x2][y2] - prefixSum[x1-1][y2] - prefixSum[x2][y1-1] + prefixSum[x1-1][y1-1];
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
             System.out.println(result);
         }
-
-        sc.close();
     }
 }
+
