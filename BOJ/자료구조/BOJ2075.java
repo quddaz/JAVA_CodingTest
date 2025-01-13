13be3692b145096ec436277981a7d83b109fb9e0
김병웅 <quddnddl35@naver.com>
2025-01-13 19:44:41 +0900

    [BOJ]2075번 N번째 큰수 우선순위큐 S3

---
+++ BOJ/자료구조/BOJ2075.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                int value = Integer.parseInt(st.nextToken());
+                pq.add(value);
+                if (pq.size() > N) {
+                    pq.poll();
+                }
+            }
+        }
+        
+        System.out.println(pq.poll());
+    }
+}
