c99a84ae9df2b40a0ed6108947d7645169383afa
김병웅 <quddnddl35@naver.com>
2025-02-11 22:18:25 +0900

    [BOJ]2493번 탑 스택 G5

---
+++ BOJ/자료구조/BOJ2493.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int top[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            top[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<int[]> s = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            while(!s.isEmpty() && s.peek()[0] < top[i]){
+                s.pop();
+            }
+
+            if(!s.isEmpty()){
+                sb.append(s.peek()[1] + 1).append(" ");
+            }else
+                sb.append(0).append(" ");
+            
+            s.push(new int[]{top[i], i});
+        }
+
+        System.out.println(sb);
+    }
+}
