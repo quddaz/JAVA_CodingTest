871178ac85e6b5c5a6db2098d3d20bc0aa32b340
김병웅 <quddnddl35@naver.com>
2025-03-24 20:50:11 +0900

    [BOJ]1719번 택배 플로이드 G3

---
+++ BOJ/플로이드/BOJ1719.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int INF = 1000000000; 
+    static int[][] dist, next;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        dist = new int[n + 1][n + 1];
+        next = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            dist[u][v] = w;
+            dist[v][u] = w; 
+            next[u][v] = v;
+            next[v][u] = u;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF && dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        next[i][j] = next[i][k]; 
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    sb.append("- ");
+                } else {
+                    sb.append(next[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
