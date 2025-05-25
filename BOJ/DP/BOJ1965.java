be5f69a4921cb132b0df26edc863bab374ed8ae7
김병웅 <quddnddl35@naver.com>
2025-05-25 23:36:54 +0900

    [BOJ]1965번 상자넣기 DP S2

---
+++ BOJ/DP/BOJ1965.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] boxes = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            boxes[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;  // 초기화
+        }
+
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (boxes[j] < boxes[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
