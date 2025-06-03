92955549218a513f924e06aec526becf59bade6f
김병웅 <quddnddl35@naver.com>
2025-06-03 21:41:34 +0900

    [BOJ]7662번 이중 우선순위 큐 트리셋 G4

---
+++ BOJ/자료구조/BOJ7662.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for (int i = 0; i < k; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String cmd = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+
+                if (cmd.equals("I")) {
+                    map.put(num, map.getOrDefault(num, 0) + 1);
+                } else {
+                    if (map.isEmpty()) continue;
+
+                    int key = (num == 1) ? map.lastKey() : map.firstKey();
+                    int count = map.get(key);
+
+                    if (count == 1) {
+                        map.remove(key);
+                    } else {
+                        map.put(key, count - 1);
+                    }
+                }
+            }
+
+            if (map.isEmpty()) {
+                sb.append("EMPTY\n");
+            } else {
+                sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
