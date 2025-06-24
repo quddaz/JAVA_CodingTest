52b95db26cb1d28e7e94313e36192c76daf79a7e
김병웅 <quddnddl35@naver.com>
2025-06-24 20:46:50 +0900

    [BOJ]14938번 서강그라운드 플로이드 G5

---
+++ BOJ/플로이드/BOJ14938.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int INF = 1000; 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken()); 
+        int r = Integer.parseInt(st.nextToken()); 
+
+        int[] items = new int[n]; 
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            items[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dist = new int[n][n]; 
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int l = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = l;
+            dist[b][a] = l;
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int maxItem = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] <= m) {
+                    sum += items[j];
+                }
+            }
+            maxItem = Math.max(maxItem, sum);
+        }
+
+        System.out.println(maxItem);
+    }
+}
