63cc216806fa3bf4a45ddc904f5ce15bb19fd787
김병웅 <quddnddl35@naver.com>
2025-09-17 21:50:57 +0900

    [Programmers]LV.3 불량사용자 백트래킹

---
+++ Programmers/카카오/ProLV3불량사용자.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    Set<Set<String>> result = new HashSet<>();
+    
+    public int solution(String[] user_id, String[] banned_id) {
+        List<List<String>> possibleBans = new ArrayList<>();
+        
+        for (String ban : banned_id) {
+            List<String> matchingUsers = new ArrayList<>();
+            for (String user : user_id) {
+                if (ban.length() == user.length() && valid(user, ban)) {
+                    matchingUsers.add(user);
+                }
+            }
+            possibleBans.add(matchingUsers);
+        }
+        
+        findCombinations(possibleBans, new HashSet<>(), 0);
+        
+        return result.size();
+    }
+    
+    private void findCombinations(List<List<String>> possibleBans, Set<String> selected, int index) {
+        if (index == possibleBans.size()) {
+            result.add(new HashSet<>(selected));
+            return;
+        }
+        
+        for (String user : possibleBans.get(index)) {
+            if (!selected.contains(user)) {
+                selected.add(user);
+                findCombinations(possibleBans, selected, index + 1);
+                selected.remove(user);
+            }
+        }
+    }
+    private boolean valid(String user, String ban) {
+        for (int i = 0; i < user.length(); i++) {
+            if (ban.charAt(i) != '*' && user.charAt(i) != ban.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
