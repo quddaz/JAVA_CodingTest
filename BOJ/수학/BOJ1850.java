f47dc7e1ba1ac775e67520de1a68b16d0d893e50
김병웅 <quddnddl35@naver.com>
2025-12-15 23:47:37 +0900

    [BOJ]1850번 최대공약수 수학 S1

---
+++ BOJ/수학/BOJ1850.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long g = gcd(a, b);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < g; i++) {
+            sb.append('1');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
